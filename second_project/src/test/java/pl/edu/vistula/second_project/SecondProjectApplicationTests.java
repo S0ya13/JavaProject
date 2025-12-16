d8a56c1dab18e7fea805d37a02641d47a14dc979
S0ya <oj.savchak@gmail.com>
2025-12-16 20:52:37 +0100

    Added empty skeleton for Task 2

---
+++ second_project/src/test/java/pl/edu/vistula/second_project/SecondProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.second_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
